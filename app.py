f9709b09414d34af87511984f036e71b016cfa08
killu4kun <mateeus.soarees@outlook.com>
2023-05-18 04:52:25 -0300

    bunch of stuff

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request,jsonify
+import json
+from flask_cors import CORS
 import psycopg2
 
 app = Flask(__name__)
+CORS(app)
 
 # Conexão com o banco de dados PostgreSQL
 conn = psycopg2.connect(
@@ -68,6 +71,32 @@ def view_webhooks():
     cursor.close()
     return str(webhooks)
 
+@app.route('/api/webhooks', methods=['GET'])
+def get_webhooks():
+    # Consulte o banco de dados para obter todos os webhooks
+    cursor = conn.cursor()
+    cursor.execute("SELECT payload->'payload' FROM webhooks")
+    rows = cursor.fetchall()
+    cursor.close()
+
+    # Transforme os resultados em uma lista de dicionários
+    webhook_data = []
+    for row in rows:
+        payload = json.loads(row[0].replace("'", "\""))  # Substitui as aspas simples por aspas duplas
+        webhook_dict = {
+            'nome': payload.get('nome'),
+            'email': payload.get('email'),
+            'status': payload.get('status'),
+            'valor': str(payload.get('valor')),  # Converta para string
+            'forma_pagamento': payload.get('forma_pagamento'),
+            'parcelas': payload.get('parcelas')
+        }
+        webhook_data.append(webhook_dict)
+
+    # Retorne os dados como uma resposta JSON
+    return jsonify(webhook_data), 200
+
+
 # Rota para pesquisar tratativas de um usuário específico
 @app.route('/user/<email>', methods=['GET'])
 def view_user(email):
@@ -79,6 +108,106 @@ def view_user(email):
     user_treatments = cursor.fetchall()
     cursor.close()
     return str(user_treatments)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()  # Obtém os dados do formulário de login
+    email = data.get('email')
+    senha = data.get('senha')
+
+    # Verifica se o usuário existe no banco de dados
+    if not is_user_registered(email):
+        return jsonify({'message': 'Usuário não encontrado'}), 404
+
+    # Obtém os dados do usuário
+    user = get_user(email)
+
+    # Verifica se os dados de e-mail e senha estão corretos
+    if user['senha'] != senha:
+        return jsonify({'message': 'Credenciais inválidas'}), 401
+
+    # Verifica se o token é válido
+    if user['token'] != 'uhdfaAADF123':
+        return jsonify({'message': 'Token inválido'}), 401
+
+    # Usuário válido, realizar o login
+    return jsonify({'message': 'Login bem-sucedido','status':'200'})
+
+# Função para obter os dados do usuário pelo e-mail
+def get_user(email):
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM usuarios WHERE email = %s",
+        (email,)
+    )
+    user = cursor.fetchone()
+    cursor.close()
+
+    # Transforma os dados do usuário em um dicionário
+    user_data = {
+        'email': user[1],
+        'senha': user[2],
+        'token': user[3]
+    }
+
+    return user_data
+
+# Função para verificar se o token é válido
+def is_valid_token(token):
+    # Implemente sua lógica para validar o token
+    # Por exemplo, você pode comparar o token recebido com um valor fixo
+    # ou consultar o banco de dados para verificar se o token é válido  
+    return token == 'uhdfaAADF123'
+
+# Função para verificar se as informações de login são válidas no banco de dados
+def is_valid_user(email, senha):
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM usuarios WHERE email = %s AND senha = %s",
+        (email, senha)
+    )
+    user = cursor.fetchone()
+    cursor.close()
+    return user is not None
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()  # Obtém os dados do formulário de cadastro
+    email = data.get('email')
+    senha = data.get('senha')
+    token = data.get('token')
+
+
+    # Verifica se o usuário já está cadastrado
+    if is_user_registered(email):
+        return jsonify({'message': 'Usuário já cadastrado',"status":'400'}), 400
+
+    # Insere o novo usuário no banco de dados
+    insert_user(email, senha,token)
+
+    # Retorna uma mensagem de sucesso
+    return jsonify({'message': 'Usuário cadastrado com sucesso'})
+
+# Função para verificar se o usuário já está cadastrado
+def is_user_registered(email):
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM usuarios WHERE email = %s",
+        (email,)
+    )
+    user = cursor.fetchone()
+    cursor.close()
+    return user is not None
+
+# Função para inserir um novo usuário no banco de dados
+def insert_user(email, senha, token):
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO usuarios (email, senha, token) VALUES (%s, %s, %s)",
+        (email, senha, token)
+    )
+    conn.commit()
+    cursor.close()
 
 if __name__ == '__main__':
     app.run()
